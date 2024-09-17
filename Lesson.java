ea8bc08b872e17c13f8bbba6a51f63a7b1cb349a
LeandroC192 <Leandroc16@nycstudents.net>
2024-09-17 00:54:32 +0000

    first commit

---
+++ Lesson.java
@@ -0,0 +1,14 @@
+public class Lesson
+{
+    public static void main(String[] args)
+    {
+        String b = "bee";
+        int i = 5;
+        boolean y = true; 
+        double e = 6.6; 
+        final double g = 7.2;
+        e += 3;  
+        String reference = "finish";
+        System.out.println(reference + i + e + g + y + b);
+    }
+}
